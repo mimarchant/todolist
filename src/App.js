b965283740c598950c616442c1a6bd972e8837fd
mimarchant <mimarchtt@gmail.com>
2021-01-23 12:01:36 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
+import Title from './components/title';
 
 function App() {
+
+
+  let inputRef = useRef(null);
+
+  const [list, setList] = useState([]);
+
+  const addTask = (e) => {
+    if (e.keyCode === 13 && inputRef.value !== "") {
+      /* setTask(inputRef.value); */
+      setList(list.concat(inputRef.value));
+      inputRef.value = "";
+
+    }
+
+  }
+  const addTask2 = () => {
+    if (inputRef.value !== "") {
+      setList(list.concat(inputRef.value));
+      inputRef.value = "";
+    }
+  }
+
+  const deleteTask = (i) => {
+    list.splice(i, 1);
+    setList([...list]);
+  }
+
+  const deleteAllTasks = () => {
+    setList([])
+  }
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container border mt-5 fondo">
+        <Title />
+        <div className="d-flex mt-5">
+          <input ref={r => inputRef = r} onKeyUp={addTask} className="col-md-9 form-control" placeholder="What needs to be done?" autoFocus></input>
+          <button onClick={addTask2} className="btn ml-5 bg-success text-white"><i class="fas fa-thumbtack"></i></button>
+        </div>
+        <div className="">
+          <ol className="list-group mb-5">
+            {
+              !!list.length > 0 &&
+              list.map((task, i) => {
+                return (
+                  <li key={i} className="col-md-9 list-group-item text-start">{task}<i onClick={() => deleteTask(i)} className="fas fa-trash"></i></li>
+                )
+              })
+            }
+
+          </ol>
+          <div className="text-center">
+            <button className="btn bg-danger mb-3" onClick={deleteAllTasks}>delete</button>
+          </div>
+
+        </div>
+
+      </div>
+    </>
   );
 }
 
